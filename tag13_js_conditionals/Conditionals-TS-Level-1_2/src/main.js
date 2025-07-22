67c247accafdc4bcc47b12097b6c800aaa9684eb
BinaryBecca <rebecca.thinnes@web.de>
2025-07-22 17:04:13 +0200

    update Aufgaben

--- tag13_js_conditionals/Conditionals-TS-Level-1_2/src/main.js
+++ tag13_js_conditionals/Conditionals-TS-Level-1_2/src/main.js
@@ -1,13 +1,14 @@
-function weather() {
-  const weatherInput = document.querySelector("#weather").valueAsNumber
+function weatherOutput() {
+  const weatherInput = document.querySelector("#weather2").value
   const result = document.querySelector(".result")
+  console.log(weatherInput)
   if (weatherInput >= 8 && weatherInput <= 10) {
-    result.innerHTML = `<p>super</p>`
+    result.innerHTML = `<p>Das Wetter ist super</p>`
   } else if (weatherInput >= 6 && weatherInput <= 7) {
-    result.innerHTML = `<p>gut</p>`
+    result.innerHTML = `<p>Das Wetter ist gut</p>`
   } else if (weatherInput >= 3 && weatherInput <= 5) {
-    result.innerHTML = `<p>okay</p>`
+    result.innerHTML = `<p>Das Wetter ist okay</p>`
   } else if (weatherInput >= 0 && weatherInput <= 2) {
-    result.innerHTML = `<p>schlecht</p>`
+    result.innerHTML = `<p>Das Wetter ist schlecht</p>`
   }
 }
---
+++ tag13_js_conditionals/Functions-Level-1_5/src/main.js
@@ -0,0 +1,11 @@
+function math(a, b) {
+  console.log("Multiplikation: " + a * b)
+  console.log("Division: " + a / b)
+}
+
+math(10, 2)
+math(30, 20)
+math(100, 100)
+math(5, 0)
+math(45, 173)
+math(1, 1000)
